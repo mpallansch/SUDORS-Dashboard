037018d688d1121577f68cd4ca0161268d8107fb
mpallansch <matthew.pallansch@gmail.com>
2021-08-30 15:07:22 -0400

    Adds Animation

--- src/components/CauseChart.js
+++ src/components/CauseChart.js
@@ -1,17 +1,27 @@
 import { useEffect } from 'react';  
 import * as d3 from 'd3';
 
-import data from '../data/type-of-drug.json';
+import data from '../data/causes.json';
 
 import '../css/CauseChart.css';
 
 function CauseChart(params) {
 
   const { width, height } = params;
-  const margin = 50;
-  const doubleMargin = margin * 2;
-  const adjustedWidth = width + doubleMargin;
-  const adjustedHeight = height + doubleMargin;
+  const margin = {top: 10, left: 70, right: 20, bottom: 70};
+  const adjustedWidth = width - margin.left - margin.right;
+  const adjustedHeight = height - margin.top - margin.bottom;
+
+  const x = d3.scaleBand()
+    .range([ 0, adjustedWidth ])
+    .domain(data.map(d => d.opioid))
+    .padding(0.2);
+  const xHalfBandwidth = x.bandwidth() / 2;
+
+  // Y scale
+  const y = d3.scaleLinear()
+    .domain([0, 100])
+    .range([ adjustedHeight, margin.top ]);
 
   useEffect(() => {
     // Gets element reference
@@ -22,31 +32,15 @@ function CauseChart(params) {
 
     // Creates new SVG element
     let svg = chartEl.append('svg')
-      .attr('width', adjustedWidth)
-      .attr('height', adjustedHeight)
-    .append('g')
-      .attr('transform',
-          'translate(' + margin + ', ' + margin + ')');;
-
-    // X scale
-    let x = d3.scaleBand()
-      .range([ 0, width - doubleMargin ])
-      .domain(data.map(d => d.opioid))
-      .padding(0.2);
-    let xHalfBandwidth = x.bandwidth() / 2;
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+        .attr('transform',
+            'translate(' + margin.left + ', ' + margin.top + ')');;
 
     // X axis
     svg.append('g')
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(d3.axisBottom(x))
-      .selectAll('text')
-        .style('text-anchor', 'middle')
-        .style('font-size', 'medium');
-
-    // Y scale
-    let y = d3.scaleLinear()
-      .domain([0, 100])
-      .range([ height, 0]);
+      .call(xAxis);
 
     // Y axis
     svg.append('g')
@@ -56,7 +50,7 @@ function CauseChart(params) {
       .attr('font-size', 'medium');
 
     // Begins adding elements to SVG
-    let bars = svg.selectAll('mybar')
+    let bars = svg.selectAll('bar')
       .data(data)
       .enter();
 
@@ -65,7 +59,7 @@ function CauseChart(params) {
         .attr('x', d => x(d.opioid))
         .attr('y', d => y(d.present))
         .attr('width', x.bandwidth())
-        .attr('height', d => height - y(d.present))
+        .attr('height', d => adjustedHeight - y(d.present))
         .attr('fill', 'rgb(198, 209, 230)');
 
     // Adds circles
@@ -81,18 +75,83 @@ function CauseChart(params) {
       .y(d => y(d.cause));
     svg.append('path')
       .attr('d', lineFunc(data))
-      .attr('stroke-width', '3')
+      .attr('strokeWidth', '3')
       .attr('stroke', 'rgb(58, 88, 161)')
       .attr('fill', 'none');
   });
 
+  const xAxis = (g) => {
+    return g.attr('class', 'x-axis')
+      .attr('transform', 'translate(0,' + adjustedHeight + ')')
+      .call(d3.axisBottom(x))
+      .selectAll('text')
+        .style('text-anchor', 'middle')
+        .style('font-size', 'medium');
+  };
+
+  const selectChange = (e) => {
+    let svg = d3.select('#cause-chart svg');
+    let gx = svg.select('.x-axis');
+    let bar = svg.selectAll('rect');
+    let circle = svg.selectAll('circle');
+    let path = svg.selectAll('path');
+
+    x.domain(data.sort((a, b) => {
+      if(e.target.value === 'one'){
+        if(a.present < b.present){
+          return 1;
+        } else if(b.present < a.present) {
+          return -1;
+        } else {
+          return 0;
+        }
+      } else {
+        if(a.present > b.present){
+          return 1;
+        } else if(b.present > a.present) {
+          return -1;
+        } else {
+          return 0;
+        }
+      }
+    }).map(d => d.opioid));
+
+    const t = svg.transition()
+        .duration(750);
+
+    bar.data(data, d => d.opioid)
+        .order()
+      .transition(t)
+        .delay((d, i) => i * 20)
+        .attr("x", d => x(d.opioid));
+
+    circle.data(data, d => d.opioid)
+        .order()
+      .transition(t)
+        .delay((d, i) => i * 20)
+        .attr("cx", d => x(d.opioid) + xHalfBandwidth);
+
+    let lineFunc = d3.line()
+      .x(d => x(d.opioid) + xHalfBandwidth)
+      .y(d => y(d.cause));
+    path
+      .attr('d', lineFunc(data))
+      .attr('strokeWidth', '3')
+      .attr('stroke', 'rgb(58, 88, 161)')
+      .attr('fill', 'none');
+
+    gx.transition(t)
+        .call(xAxis)
+      .selectAll(".tick")
+        .delay((d, i) => i * 20);
+  };
+
   return (
     <>
-      <h2 id="cause-chart-header">What drugs<sup>1</sup> were identified?</h2>
-      <div id="cause-chart-legend">
-        <span><svg id="present-indicator"><rect width="100%" height="100%" fill="rgb(198, 209, 230)" /></svg>% with drug present</span>
-        <span><svg id="cause-indicator" viewBox="0 0 100 100"><circle cx="50" cy="50" r="35" fill="rgb(58, 88, 161)"></circle><line x1="0" x2="100" y1="50" y2="50" stroke-width="25" stroke="rgb(58, 88, 161)"/></svg>% with drug listed as cause of death</span>
-      </div>
+      <select onChange={selectChange}>
+        <option>one</option>
+        <option>two</option>
+      </select>
       <div id="cause-chart"></div>
     </>
   );
