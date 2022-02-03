cae1238d014516f2562ef14785ca512b05a34bb1
mpallansch <matthew.pallansch@gmail.com>
2022-02-03 15:51:25 -0500

    Updates Circumstances Calculation

--- data-processor/process.js
+++ data-processor/process.js
@@ -9,7 +9,7 @@ const weights = require('./census/weights.json');
 const fips = {"0":"Overall", "1": "Alabama","2":"Alaska","4":"Arizona","5":"Arkansas","6":"California","8":"Colorado","9":"Connecticut","10":"Delaware", "11": "District of Columbia", "12": "Florida", "13": "Georgia", "15": "Hawaii", "16": "Idaho", "17": "Illinois", "18": "Indiana", "19": "Iowa", "20": "Kansas", "21": "Kentucky", "22": "Louisiana", "23": "Maine", "24": "Maryland", "25": "Massachusetts", "26": "Michigan", "27": "Minnesota", "28": "Mississippi", "29": "Missouri", "30": "Montana", "31": "Nebraska", "32": "Nevada", "33": "New Hampshire", "34": "New Jersey", "35": "New Mexico", "36": "New York", "37": "North Carolina", "38": "North Dakota", "39": "Ohio", "40": "Oklahoma", "41": "Oregon", "42": "Pennsylvania", "44": "Rhode Island", "45": "South Carolina", "46": "South Dakota", "47": "Tennessee", "48": "Texas", "49": "Utah", "50": "Vermont", "51": "Virginia", "53": "Washington", "54": "West Virginia", "55": "Wisconsin", "56": "Wyoming"};
 const reverseFips = {"Overall":"0","Alabama":"1","Alaska":"2","Arizona":"4","Arkansas":"5","California":"6","Colorado":"8","Connecticut":"9","Delaware":"10","District of Columbia":"11","Florida":"12","Georgia":"13","Hawaii":"15","Idaho":"16","Illinois":"17","Indiana":"18","Iowa":"19","Kansas":"20","Kentucky":"21","Louisiana":"22","Maine":"23","Maryland":"24","Massachusetts":"25","Michigan":"26","Minnesota":"27","Mississippi":"28","Missouri":"29","Montana":"30","Nebraska":"31","Nevada":"32","New Hampshire":"33","New Jersey":"34","New Mexico":"35","New York":"36","North Carolina":"37","North Dakota":"38","Ohio":"39","Oklahoma":"40","Oregon":"41","Pennsylvania":"42","Rhode Island":"44","South Carolina":"45","South Dakota":"46","Tennessee":"47","Texas":"48","Utah":"49","Vermont":"50","Virginia":"51","Washington":"53","West Virginia":"54","Wisconsin":"55","Wyoming":"56"};
 
-const inputFilePath = './SUDORS 2020 Prelim Data for Dashboard_21JAN2022.csv';
+const inputFilePath = './SUDORS 2020 Prelim Data for Dashboard_01FEB2022.csv';
 const typeOfDrugFilePath = '../src/data/causes.json';
 const additionalDrugFilePath = '../src/data/additional-drugs.json';
 const circumstancesFilePath = '../src/data/circumstances.json';
@@ -65,7 +65,8 @@ const keys = [
   'opioid_r_cod',
   'opioid_r_pos',
   'stimulant_cod',
-  'stimulant_pos'
+  'stimulant_pos',
+  'combo_cme_flag'
 ];
 const drugTypeMapping = {
   'meth_r_cod': 'meth_r',
@@ -498,49 +499,49 @@ fs.createReadStream(inputFilePath)
         intervention: [
           { 
             circumstance: 'Recent release from institutional setting', 
-            percent: percent(keyCounts[state]['recentinst']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['recentinst']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['recentinst']['1']) },
           { 
             circumstance: 'Prior overdose', 
-            percent: percent( keyCounts[state]['priorod']['1'], totalDeaths[state]),
+            percent: percent( keyCounts[state]['priorod']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['priorod']['1']) },
           { 
             circumstance: 'Mental health diagnosis', 
-            percent: percent(keyCounts[state]['MentalHealthProblem_c']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['MentalHealthProblem_c']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['MentalHealthProblem_c']['1']) },
           { 
             circumstance: 'Current treatment for substance use disorder', 
-            percent: percent(keyCounts[state]['evertrt']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['evertrt']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['evertrt']['1']) },
           { 
             circumstance: 'Potential bystander present', 
-            percent: percent(keyCounts[state]['bystander']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['bystander']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['bystander']['1']) },
           { 
             circumstance: 'Fatal drug use witnessed', 
-            percent: percent(keyCounts[state]['witnesseddruguse']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['witnesseddruguse']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['witnesseddruguse']['1']) },
         ],
         other: [
           { 
             circumstance: 'History of substance use/misuse', 
-            percent: percent(keyCounts[state]['SubstanceAbuseOther_c']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['SubstanceAbuseOther_c']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['SubstanceAbuseOther_c']['1']) },
           { 
             circumstance: 'Naloxone administered', 
-            percent: percent(keyCounts[state]['NaloxoneAdministered']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['NaloxoneAdministered']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['NaloxoneAdministered']['1']) },
           { 
             circumstance: 'Current pain treatment', 
-            percent: percent(keyCounts[state]['pain_treat']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['pain_treat']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['pain_treat']['1']) },
           { 
             circumstance: 'Recent return to use of opioids', 
-            percent: percent(keyCounts[state]['recentrelapse']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['recentrelapse']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['recentrelapse']['1']) },
           { 
             circumstance: 'Experiencing homelessness', 
-            percent: percent(keyCounts[state]['Homeless']['1'], totalDeaths[state]),
+            percent: percent(keyCounts[state]['Homeless']['1'], keyCounts[state]['combo_cme_flag']['0']),
             count: checkCutoff(keyCounts[state]['Homeless']['1']) }
         ]
       };
